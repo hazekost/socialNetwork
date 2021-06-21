5bb9a9b06d5cf4a7930e2a6ec98d29d1e3473e68
hazekost <kostyk0007@gmail.com>
2021-06-21 08:39:44 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {Navbar} from "./components/Navbar/Navbar";
-import {Route, withRouter} from "react-router-dom";
-import HeaderContainer from "./components/Header/HeaderConstainer";
-import {connect} from 'react-redux';
-import {compose} from "redux";
-import {RouteComponentProps} from "react-router";
-import {initializeAppTC} from "./Redux/app-reducer";
-import {rootStateType} from "./Redux/reduxStore";
-import {Preloader} from "./components/common/Preloader/Preloader";
-import {WithSuspense} from "./hoc/withSuspense";
 
-// import DialogsContainer from "./components/Dialogs/DialogsContainer";
-// import ProfileContainer from "./components/Profile/ProfileContainer";
-// import UsersContainer from "./components/Users/UsersContainer";
-// import Login from "./components/Login/Login";
-
-const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
-const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"))
-const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"))
-const Login = React.lazy(() => import("./components/Login/Login"))
-
-type AppPropsType = {
-    initializeAppTC: () => void
-    initialized: boolean
-}
-type PropsType = RouteComponentProps & AppPropsType
-
-class App extends React.Component<PropsType> {
-
-    componentDidMount() {
-        this.props.initializeAppTC()
-    }
-
-    render() {
-        if (!this.props.initialized) {
-            return <Preloader/>
-        }
-        return (
-            <div className={"app"}>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className={"app-content"}>
-                    <Route path={"/profile/:userId?"} render={WithSuspense(ProfileContainer)}/>
-                    <Route path={"/dialogs"} render={WithSuspense(DialogsContainer)}/>
-                    <Route path={"/users"} render={WithSuspense(UsersContainer)}/>
-                    <Route path={"/login"} render={WithSuspense(Login)}/>
-                </div>
-            </div>
-        )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-const mapStateToProps = (state: rootStateType) => {
-    return {
-        initialized: state.app.initialized
-    }
-}
-
-export default compose<React.ComponentType>(withRouter, connect(mapStateToProps, {initializeAppTC}))(App);
+export default App;
--- src/components/common/Preloader/Preloader.tsx
+++
@@ -1,7 +0,0 @@
-import preloader from "../../../assets/images/J7eF9.gif";
-import React from "react";
-import s from "./Preloader.module.css"
-
-export const Preloader = () => {
-    return <div><img className={s.preload} src={preloader}/></div>
-}
